83cd5c691edd092ea4296ec2bcc0813510dbcb18
SpheneDev <devsphene@gmail.com>
2025-11-27 06:35:44 +0100

    Fix for Mcdf Files could not be loaded

--- Sphene/Services/CharaData/Models/SpheneCharaFileHeader.cs
+++ Sphene/Services/CharaData/Models/SpheneCharaFileHeader.cs
@@ -10,7 +10,7 @@ public record SpheneCharaFileHeader(byte Version, SpheneCharaFileData CharaFileD
 
     public void WriteToStream(BinaryWriter writer)
     {
-        writer.Write('M');
+        writer.Write('S');
         writer.Write('C');
         writer.Write('D');
         writer.Write('F');
@@ -23,7 +23,7 @@ public record SpheneCharaFileHeader(byte Version, SpheneCharaFileData CharaFileD
     public static SpheneCharaFileHeader? FromBinaryReader(string path, BinaryReader reader)
     {
         var chars = new string(reader.ReadChars(4));
-        if (!string.Equals(chars, "SCDF", StringComparison.Ordinal)) throw new InvalidDataException("Not a Sphene Chara File");
+        if (!(string.Equals(chars, "SCDF", StringComparison.Ordinal) || string.Equals(chars, "MCDF", StringComparison.Ordinal))) throw new InvalidDataException("Not a Sphene Chara File");
 
         SpheneCharaFileHeader? decoded = null;
 
